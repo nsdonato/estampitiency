671053242b0a2ab4aabb46f4fb5d38c9ed40481c
Noelia Donato <noeliadonato@gmail.com>
2022-03-24 19:52:33 -0300

    feat(styledComponents): add porting styles to styled components

---
+++ src/App.tsx
@@ -0,0 +1,51 @@
+import { useEffect, useState } from "react";
+import api from "./api";
+import { Product } from "./types";
+
+import {
+  Aside,
+  Button,
+  Img,
+  Article,
+  Footer,
+  Section,
+  Header,
+  Main,
+} from "./components";
+
+function App() {
+  const [products, setProducts] = useState<Product[]>([]);
+
+  useEffect(() => {
+    api.list().then(setProducts);
+  }, []);
+
+  return (
+    <Main>
+      <Header>Estampitiency</Header>
+      <Section>
+        {products.map((product) => (
+          <Article key={product.id}>
+            <Img src={product.image} />
+            <div>
+              <p>{product.title}</p>
+              <p>{product.description}</p>
+            </div>
+            <Button>Agregar</Button>
+          </Article>
+        ))}
+      </Section>
+      <Aside>
+        <Button>3 productos (total: $12)</Button>
+      </Aside>
+      <Footer>
+        Encontrá la consigna de este ejercicio y otros más{" "}
+        <a href="https://github.com/goncy/interview-challenges/tree/main/simple-cart">
+          acá
+        </a>
+      </Footer>
+    </Main>
+  );
+}
+
+export default App;
---
+++ src/components/index.tsx
@@ -0,0 +1,10 @@
+import { Aside } from "./Aside";
+import { Button } from "./Button";
+import { Img } from "./Img";
+import { Article } from "./Article";
+import { Footer } from "./Footer";
+import { Section } from "./Section";
+import { Header } from "./Header";
+import { Main } from "./Main";
+
+export { Aside, Button, Img, Article, Footer, Section, Header, Main };
